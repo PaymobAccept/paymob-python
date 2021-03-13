983539be8003bdadcb6ba52dbd318dcb9589fc0f
misraX <maysragamal@gmail.com>
2021-03-13 20:17:52 +0200

    HTTPResource HTTPResourceMixin GenericHTTPResourceMixin Adding test cases for resources and mixins.

--- paymob/utils.py
+++ paymob/utils.py
@@ -1,19 +1,28 @@
-from paymob import API_VERSION
+from paymob import api_version
 
 
-def next_api_version(version: str = None) -> str:
+def next_api_version(version=None):
     """
     Next API version handler.
     :param version: str
     :return: str
     """
-    return API_VERSION if not version else "v1"
+    return api_version if not version else "v1"
 
 
-def resource_to_url(resource: str) -> str:
+def resource_to_url(resource):
     """
     Convert any Resource's classes name to a valid API URL.
     :param resource: str
     :return: str
     """
     return f"/{next_api_version()}/{resource}/"
+
+
+def api_base_url():
+    """
+
+    :return: str api_next_url
+    """
+    api_next_url: str = "http://127.0.0.1:49152/api/next"
+    return api_next_url
---
+++ tests/test_http.py
@@ -0,0 +1,30 @@
+import pytest
+
+import paymob
+from paymob.http import GenericHTTPResource
+
+
+@pytest.fixture(autouse=True)
+def setup_next():
+    paymob.secret_key = "HelloWorld"
+
+
+class IntentionResource(GenericHTTPResource):
+    RESOURCE_URI = "intention"
+
+
+class IntentionResourceWithoutURI(GenericHTTPResource):
+    pass
+
+
+class TestHttpResources(object):
+    def test_generic_http_resource_url(self):
+        intention = IntentionResource()
+        assert intention.resource_url() == "/v1/intention/"
+
+    def test_generic_http_resource_url_exception(self):
+        intention = IntentionResourceWithoutURI()
+        with pytest.raises(NotImplementedError) as exception:
+            intention.resource_url()
+
+            assert "must have a RESOURCE_URI string." in exception.value
