919f527c46e74bab5d85f35493bc73b2195c8f76
maysra <maysragamal@paymobsolutions.com>
2021-09-21 14:26:57 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='paymob',
+      version='1.0',
+      description='Your Paymob SDK in python',
+      author='Paymob',
+      author_email='maysragamal@paymob.com',
+      url='',
+      packages=['paymob'],
+      install_requires=['requests']
+     )
