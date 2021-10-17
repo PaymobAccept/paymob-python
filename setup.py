38b4f8b98eb985e10531fd1a7e41f66aeffe5941
maysra <maysragamal@paymobsolutions.com>
2021-10-17 16:50:59 +0200

    Fixing package setup to include sub dirs.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='paymob',
-      version='1.0',
+      version='1.0.1',
       description='Your Paymob SDK in python',
       author='Paymob',
       author_email='maysragamal@paymob.com',
       url='https://github.com/PaymobAccept/paymob-python',
-      packages=['paymob'],
+      packages=find_packages(exclude=["tests"]),
       install_requires=['requests']
      )
